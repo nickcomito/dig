a26eda3b6947e09760046cd23ee7fd3e02770b0b
Nick Comito <nick.comito@praxent.com>
2016-05-16 09:06:01 -0500

    remove old fonts. Add Google material icon font for icons instead of svg

--- icons.php
+++ icons.php
@@ -2,8 +2,8 @@
 <html class="no-js" lang="en">
   <head>
     <title>Icons</title>
-    <link rel="stylesheet" href="stylesheets/screen.css">
-    <script data-main="js/ui.js" src="js/require.js"></script>
+    <?php include('includes/stylesheets.inc'); ?>
+    <?php include('includes/meta.inc'); ?>
   </head>
   <body>
     <div class="contain">
@@ -14,41 +14,24 @@
       </div>
       <div class="sm-4 md-8 lg-9 centered">
         <hr>
-        <a id="tutorial-trigger" class="right">tutorial</a>
         <h1 class="margbot-5">Icons</h1>
       </div>
       <div class="sm-4 md-8 lg-9 lg-centered">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl fill-none stroke-primary">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl fill-none stroke-accent">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl outline stroke-tertiary">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl fill-primary">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl fill-accent">
-        <img src="images/icon_star.svg" class="svg icon icon-star xxl fill-tertiary">
+        <a href="https://design.google.com/icons/">View full icon list</a>
 
         <br>
-
-        <img src="images/icon_info.svg" class="svg icon icon-info xxl fill-outer-dark fill-inner-white">
-        <img src="images/icon_info.svg" class="svg icon icon-info xxl fill-outer-primary fill-inner-dark">
-        <img src="images/icon_info.svg" class="svg icon icon-info xxl fill-outer-accent-alt fill-inner-white">
-        <img src="images/icon_info.svg" class="svg icon icon-info xxl fill-outer-light fill-inner-accent">
-
-        <br>
-
-        <img src="images/icon_close.svg" class="svg icon icon-close xxl fill-dark">
-        <img src="images/icon_close.svg" class="svg icon icon-close xxl fill-primary">
-        <img src="images/icon_close.svg" class="svg icon icon-close xxl fill-accent-alt">
-        <img src="images/icon_close.svg" class="svg icon icon-close xxl fill-accent">
-
-        <br>
-
-        <img src="images/icon_pencil.svg" class="svg icon icon-pencil xxl fill-dark">
-        <img src="images/icon_pencil.svg" class="svg icon icon-pencil xxl fill-primary">
-        <img src="images/icon_pencil.svg" class="svg icon icon-pencil xxl fill-accent-alt">
-        <img src="images/icon_pencil.svg" class="svg icon icon-pencil xxl fill-secondary">
+        <i>face</i>
+        <i>notifications off</i>
+        <i>build</i>
+        <i>local airport</i>
+        <i>add location</i>
+        <i>system update</i>
+        <i>refresh</i>
+        <i>arrow back</i>
+        <i>apps</i>
+        <i>menu</i>
+        <i>cancel</i>
       </div>
-    </div>
-    <div id="tutorial-overlay">
-      <div class="island"><iframe src="https://player.vimeo.com/video/14396098" width="800" height="500" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe></div>
     </div>
     <?php include('includes/scripts.inc'); ?>
   </body>
